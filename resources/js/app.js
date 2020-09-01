aa4ad9c782b450615406b1138d0fbfad97a78686
kanishak-kapur <67895454+kanishak-kapur@users.noreply.github.com>
2020-09-01 22:03:45 +0530

    add to cart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,33 @@
-console.log("Hello from Laravel");
+import axios from "axios";
+import Noty from "noty";
+let addToCart = document.querySelectorAll(".add-to-cart");
+let cartCounter = document.getElementById("cartCounter");
+
+addToCart.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    console.log(e);
+    let pizza = JSON.parse(btn.dataset.pizza);
+    updateCart(pizza);
+  });
+});
+
+function updateCart(pizza) {
+  axios
+    .post("/update-cart", pizza)
+    .then((res) => {
+      debugger;
+      cartCounter.innerText = res.data.totalQty;
+      new Noty({
+        type: "success",
+        timeout: 1000,
+        text: "Item added to cart",
+      }).show();
+    })
+    .catch((err) => {
+      new Noty({
+        type: "error",
+        timeout: 1000,
+        text: "Something went wrong!",
+      }).show();
+    });
+}
